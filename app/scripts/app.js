1d9769f7baca561f8ca37fb38935748bfa4f3a64
raziyeh bazargan <r.bazargan@gmail.com>
2016-08-07 17:36:27 -0700

    angular form validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
   angular.module('confusionApp',[])
 	  .controller('MenuController', ['$scope',function($scope) {
 			$scope.tab = 1;
@@ -66,4 +64,38 @@
 				$scope.showDetails = !$scope.showDetails;
 		  	};
 
-        }]);
+        }])
+
+  	  
+	   .controller('ContactController', ['$scope', function($scope) {
+			$scope.feedback = 
+				{
+				mychannel:"",
+				firstName:"",
+				lastName:"",
+				agree:false,
+				email:"" 
+			};
+			var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+			$scope.channels = channels;
+			$scope.invalidChannelSelection = false;
+	}])
+
+	.controller('FeedbackController', ['$scope', function($scope) {
+		$scope.sendFeedback = function() {
+        console.log($scope.feedback);
+		if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
+			 $scope.invalidChannelSelection = true;
+              console.log('incorrect');
+         }else {
+				$scope.invalidChannelSelection = false;
+				$scope.feedback = {mychannel:"", firstName:"", lastName:"",
+								   agree:false, email:"" };
+				$scope.feedback.mychannel="";
+
+				$scope.feedbackForm.$setPristine();
+				console.log($scope.feedback);
+                }
+            };
+        }])
+  ;
