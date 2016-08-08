4a5c206bffc0716d116dcff89c1805cabbafbc10
raziyeh bazargan <r.bazargan@gmail.com>
2016-08-07 19:27:48 -0700

    using factory and service

---
+++ app/scripts/controllers.js
@@ -0,0 +1,110 @@
+'use strict';
+
+//angular.module('confusionApp', [])  --> for using dependencies injection,services and factories we need to change module and remove[]
+angular.module('confusionApp')
+
+		//Now we do dependency injection to introduce the menuFactory service into MenuController:
+        .controller('MenuController', ['$scope','menuFactory', function($scope, menuFactory) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+			
+			//we moved the dishes array from here to dervice.js file, and here we use that using service name(menuFactory)
+      		$scope.dishes= menuFactory.getDishes();
+			
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope','menuFactory', function($scope, menuFactory) {
+
+			//we removed the dish array from here to and moved to service.js file and get dish using service(menuFactory)
+			$scope.dish= menuFactory.getDish(3);
+                        
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+            
+            //Step 1: Create a JavaScript object to hold the comment from the form
+            $scope.newComment = {
+				rating:'5',
+				comment:'',
+				author:'',
+				date:''
+			};
+            $scope.submitComment = function () {
+                
+                //Step 2: This is how you record the date
+                //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
+                $scope.newComment.date = new Date().toISOString();
+				
+                // Step 3: Push your comment into the dish's comment array
+                $scope.dish.comments.push($scope.newComment);
+                
+                //Step 4: reset your form to pristine
+				$scope.commentForm.$setPristine();
+				//Step 5: reset your JavaScript object that holds your comment
+				$scope.newComment = {
+					rating:'5',
+					comment:'',
+					author:'',
+					date:''
+				};
+			};
+               
+        }])
+;
